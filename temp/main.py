167a72387484c8f2cc1fa13b68dff789e3829a0d
cclngit <38783129+cclngit@users.noreply.github.com>
2024-02-29 12:44:49 +0100

    multiprocessing test

--- temp/main.py
+++ temp/main.py
@@ -1,48 +1,69 @@
-import ctranslate2
-import transformers
 import librosa
+import transformers
+import ctranslate2
+import datetime
+import os
+import glob
+import multiprocessing
 
-# Load and resample the audio file.
-audio, _ = librosa.load("audios/POV_Mini-Stories.mp3", sr=16000, mono=True)
+def transcribe_audio(audio_file):
+    try:
+        # Load the model on CPU.
+        model = ctranslate2.models.Whisper("./models/faster-whisper-tiny")
 
-# Compute the features of the first 30 seconds of audio.
-processor = transformers.WhisperProcessor.from_pretrained("openai/whisper-tiny")
-inputs = processor(audio, return_tensors="np", sampling_rate=16000)
-features = ctranslate2.StorageView.from_array(inputs.input_features)
+        # Load and resample the audio file.
+        audio, _ = librosa.load(audio_file, sr=16000, mono=True)
 
-# Load the model on CPU.
-model = ctranslate2.models.Whisper("models/faster-whisper-tiny")
+        # Compute the features of the first 30 seconds of audio.
+        processor = transformers.WhisperProcessor.from_pretrained("openai/whisper-tiny")
+        inputs = processor(audio, return_tensors="np", sampling_rate=16000)
+        features = ctranslate2.StorageView.from_array(inputs.input_features)
 
-# Detect the language.
-results = model.detect_language(features)
-language, probability = results[0][0]
-print("Detected language %s with probability %f" % (language, probability))
+        # Detect the language.
+        results = model.detect_language(features)
+        language, probability = results[0][0]
+        print("Detected language %s with probability %f" % (language, probability))
 
-# Describe the task in the prompt.
-# See the prompt format in https://github.com/openai/whisper.
-prompt = processor.tokenizer.convert_tokens_to_ids(
-    [
-        "<|startoftranscript|>",
-        language,
-        "<|transcribe|>",
-        "<|notimestamps|>",  # Remove this token to generate timestamps.
-    ]
-)
+        # Describe the task in the prompt.
+        # See the prompt format in https://github.com/openai/whisper.
+        prompt = processor.tokenizer.convert_tokens_to_ids(
+            [
+                "",
+                language,
+                "",
+                "",  # Remove this token to generate timestamps.
+            ]
+        )
 
-# Run generation for the 30-second window.
-results = model.generate(features, [prompt])
-transcription = processor.decode(results[0].sequences_ids[0])
-print(transcription)
+        # Run generation for the 30-second window.
+        results = model.generate(features, [prompt])
+        transcription = processor.decode(results[0].sequences_ids[0])
 
-src_lang = "eng_Latn"
-tgt_lang = "fra_Latn"
+        filename = datetime.datetime.now().strftime("%Y-%m-%d_%H-%M-%S") + ".txt"
+        with open(f"./transcriptions/{filename}", "w") as f:
+            f.write(transcription)
 
-translator = ctranslate2.Translator("models/nllb-200-600M-onmt", device="cpu")
-tokenizer = transformers.AutoTokenizer.from_pretrained("facebook/nllb-200-distilled-600M", src_lang=src_lang)
+        print(f"Transcription saved for {audio_file}")
+    except Exception as e:
+        print(f"Error transcribing {audio_file}: {e}")
 
-source = tokenizer.convert_ids_to_tokens(tokenizer.encode(transcription))
-target_prefix = [tgt_lang]
-results = translator.translate_batch([source], target_prefix=[target_prefix])
-target = results[0].hypotheses[0][1:]
+if __name__ == '__main__':
+    # find most recent files in a directory
+    recordings_dir = os.path.join('./recordings', '*')
+    transcribed = []
 
-print(tokenizer.decode(tokenizer.convert_tokens_to_ids(target)))
+    try:
+        while True:
+            files = sorted(glob.iglob(recordings_dir), key=os.path.getctime, reverse=True)
+            if len(files) < 1:
+                continue
+            
+            latest_recording = files[0]
+
+            if os.path.exists(latest_recording) and latest_recording not in transcribed:
+                p = multiprocessing.Process(target=transcribe_audio, args=(latest_recording,))
+                p.start()
+                transcribed.append(latest_recording)
+                
+    except KeyboardInterrupt:
+        print('Transcription stopped')
